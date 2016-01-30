cae2eeb12bb16227834d48cdbab6d33c128eba2b
Eetu Eskelinen <edwardtehgreat@gmal.com>
2016-01-30 22:08:08 +0200

    Added muzzle flash! Woah!

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,11 +6,12 @@ public class PlayerScript : MonoBehaviour
 	public GameObject bulletPrefab;
 	public float bulletSpeed = 10.0f;
 	public static int enemiesKilled = 0;
+	public ParticleSystem muzzleFlash;
 	// Use this for initialization
 
 	void Start () 
 	{
-	
+		muzzleFlash.emissionRate = 0;
 	}
 	
 	// Update is called once per frame
@@ -19,6 +20,11 @@ public class PlayerScript : MonoBehaviour
 		if(Input.GetKeyDown(KeyCode.UpArrow))
 		{
 			Fire ();
+			muzzleFlash.Emit (30);
+		}
+		else
+		{
+			muzzleFlash.emissionRate = 0;
 		}
 	}
 
