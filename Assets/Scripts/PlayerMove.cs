72650f092bda05afec42d04fc47ca7f405c9db2f
Eetu Eskelinen <edwardtehgreat@gmal.com>
2016-01-30 20:01:24 +0200

    Modified the scene and player script

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,23 +4,32 @@ using System.Collections;
 public class PlayerMove : MonoBehaviour 
 {
 	public float rotationSpeed = 100.0f;
+	GameObject player;
+	Animator playerAnim;
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+		player = GameObject.Find ("Player");
+		playerAnim = player.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if(Input.GetKey(KeyCode.LeftArrow))
+		if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
 		{
 			RotateLeft();
+
 		}
-		if(Input.GetKey(KeyCode.RightArrow ))
+		if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.W))
 		{
 			RotateRight();
+
+		}
+		else
+		{
+			playerAnim.SetInteger("Direction", 0);
 		}
 
 
@@ -29,9 +38,11 @@ public class PlayerMove : MonoBehaviour
 	void RotateLeft ()
 	{
 		transform.Rotate (Vector3.forward * rotationSpeed);
+		//playerAnim.SetInteger("Direction", 1);
 	}
 	void RotateRight()
 	{
 		transform.Rotate (Vector3.back * rotationSpeed);
+		//playerAnim.SetInteger("Direction", 1);
 	}
 }
